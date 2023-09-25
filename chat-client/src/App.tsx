05eccf959ad8b86cfd78df640296aa4399997b65
Ariel <arielah1997@gmail.com>
2023-09-25 15:45:12 +0300

    Prod secure socket url

--- chat-client/src/App.tsx
+++ chat-client/src/App.tsx
@@ -1,10 +1,9 @@
 import Layout from "./layout/layout";
 import StateProvider from "./components/providers/state.provider";
 import AppWrapper from "./screens/app-wrapper";
-
-const socketServer =
-  import.meta.env.VITE_SOCKET_SERVER ?? "ws://localhost:6969";
-export const socket = new WebSocket(socketServer);
+const wsProtocol = window.location.protocol === "https:" ? "wss" : "ws";
+const socketServer = import.meta.env.VITE_SOCKET_SERVER ?? "localhost:6969";
+export const socket = new WebSocket(`${wsProtocol}://${socketServer}`);
 
 function App() {
   return (
